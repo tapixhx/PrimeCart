c1d235290d798670a5ac962db65d0c6dc997b359
tapixhx <tapish818@gmail.com>
2020-12-10 15:09:16 +0530

    Items card added to main screen

---
+++ primecart/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PrimeCart',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.redAccent,
+        fontFamily: 'Montserrat',
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
