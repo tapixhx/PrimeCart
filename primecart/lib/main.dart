c4a861ba7217eefd5d4e35f61986bbff9df138fa
tapixhx <tapish818@gmail.com>
2020-12-11 09:28:07 +0530

    Added providers

--- primecart/lib/main.dart
+++ primecart/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,14 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PrimeCart',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Colors.redAccent,
-        fontFamily: 'Montserrat',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+          child: MaterialApp(
+        title: 'PrimeCart',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          accentColor: Colors.redAccent,
+          fontFamily: 'Montserrat',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        }
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
