373e292807a4b31b50550a4a5a8aeca5a7e4e3c4
tapixhx <tapish818@gmail.com>
2020-12-18 13:12:12 +0530

    Favorite items implemented

--- primecart/lib/screens/product_detail_screen.dart
+++ primecart/lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,9 +12,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String; // gives the id
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).finById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
--- primecart/lib/widgets/product_item.dart
+++ primecart/lib/widgets/product_item.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-          child: GridTile(
-        child: Container(
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName,
-                arguments: id,
-              );
-            },
-              child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
+        borderRadius: BorderRadius.circular(10),
+            child: GridTile(
+          child: Container(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed(
+                  ProductDetailScreen.routeName,
+                  arguments: product.id,
+                );
+              },
+                child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black38,
+            leading: Consumer<Product>(
+      builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),),
+            title: Text(
+              product.title, 
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
             ),
           ),
         ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black38,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
-          ),
-          title: Text(
-            title, 
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
-          ),
-        ),
-      ),
     );
   }
 }
