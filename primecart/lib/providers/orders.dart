f3a53b08c4ea68cfca919962f5d3fbe864628cbd
tapixhx <tapish818@gmail.com>
2020-12-21 21:24:06 +0530

    Sending orders

--- primecart/lib/providers/orders.dart
+++ primecart/lib/providers/orders.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import './cart.dart';
+import './products.dart';
 
 class orderItem {
   final String id;
@@ -23,8 +26,27 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(0, orderItem(id: DateTime.now().toString(), amount: total, products: cartProducts, dateTime: DateTime.now(),),);
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timeStamp = DateTime.now();
+    final response = await http.post(
+      Products().url + '/orders.json',
+      body: json.encode({
+        'amount': total,
+        'dataTime': timeStamp.toIso8601String(),
+        'products': cartProducts.map((cp) => {
+          'id': cp.id,
+          'title': cp.title,
+          'quantity': cp.quantity,
+          'price': cp.price,
+        }).toList(),
+      })
+    );
+    _orders.insert(0, orderItem(
+      id: json.decode(response.body)['name'], 
+      amount: total, 
+      products: cartProducts, 
+      dateTime: timeStamp,
+    ),);
     notifyListeners();
   }
 }
